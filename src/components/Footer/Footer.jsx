efa2d62624debea4b01f78a9eaaba2bbb8e65c1d
Abdelrahman <119765066+Stolir@users.noreply.github.com>
2026-04-07 15:18:43 +0300

    Write Footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>© 2026 Stolir </p>
+    </footer>
+  );
+}
+
+export default Footer;
