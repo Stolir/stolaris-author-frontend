14ff355a764aa290affa17478c5d64f4f90e37c2
Abdelrahman <119765066+Stolir@users.noreply.github.com>
2026-04-24 16:54:34 +0300

    Write a popup component to confirm destructive actions

---
+++ src/components/ConfirmPopup/ConfirmPopup.jsx
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+import styles from "./ConfirmPopup.module.css";
+
+function ConfirmPopup({ onConfirm, onClose, children }) {
+  return (
+    <div
+      role="alertdialog"
+      aria-label="Confirm choice"
+      className={styles.confirmPopup}
+    >
+      <p>{children}</p>
+      <div className={styles.controls}>
+        <button
+          type="button"
+          onClick={() => {
+            onConfirm();
+            onClose();
+          }}
+        >
+          Confirm
+        </button>
+        <button type="button" onClick={onClose}>
+          Cancel
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default ConfirmPopup;
