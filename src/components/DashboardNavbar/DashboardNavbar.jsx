08c7f136148f386d99089078b1d666ee4d79c9e8
Abdelrahman <119765066+Stolir@users.noreply.github.com>
2026-04-28 15:22:35 +0300

    Change dashboard navbar to only show search bar on the library page

--- src/components/DashboardNavbar/DashboardNavbar.jsx
+++ src/components/DashboardNavbar/DashboardNavbar.jsx
@@ -1,12 +1,19 @@
+import { useLocation } from "react-router";
 import LogoutButton from "../LogoutButton/LogoutButton";
 import SearchInput from "../SearchInput/SearchInput";
 import styles from "./DashboardNavbar.module.css";
 
 function DashboardNavbar({ searchValue, searchOnChange }) {
+  const location = useLocation();
+
   return (
     <header className={styles.header}>
-      <SearchInput value={searchValue} onChange={searchOnChange} />
-      <LogoutButton />
+      {location.pathname.includes("library") && (
+        <SearchInput value={searchValue} onChange={searchOnChange} />
+      )}
+      <div className={styles.logoutButton}>
+        <LogoutButton />
+      </div>
     </header>
   );
 }
