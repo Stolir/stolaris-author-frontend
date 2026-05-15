0792b4463361dd19cb167a1eae81badb064b21ad
Abdelrahman <119765066+Stolir@users.noreply.github.com>
2026-05-15 13:08:49 +0300

    Write utility to deep-compare objects

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -13,3 +13,32 @@ export function formatDateLocal(iso) {
     String(date.getDate()).padStart(2, "0")
   );
 }
+
+export function isDeepEqual(obj1, obj2) {
+  console.log(obj1, obj2);
+  if (obj1 === obj2) return true;
+
+  if (Number.isNaN(obj1) && Number.isNaN(obj2)) return true;
+
+  // Since primitive types are handled by the first if statement
+  // return false if either of the arguments are not objects or null
+  if (
+    typeof obj1 !== "object" ||
+    obj1 === null ||
+    typeof obj2 !== "object" ||
+    obj2 === null
+  ) {
+    return false;
+  }
+  const obj1Keys = Object.keys(obj1);
+  const obj2Keys = Object.keys(obj2);
+
+  if (obj1Keys.length !== obj2Keys.length) return false;
+
+  for (let key of obj1Keys) {
+    if (!obj2Keys.includes(key) || !isDeepEqual(obj1[key], obj2[key])) {
+      return false;
+    }
+  }
+  return true;
+}
