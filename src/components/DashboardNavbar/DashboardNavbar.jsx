641aea192915fed1f6115f4d80678f47cbbbb8b8
Abdelrahman <119765066+Stolir@users.noreply.github.com>
2026-06-12 14:35:31 +0300

    Change DashboardNavbar to display search on all pages except settings

--- src/components/DashboardNavbar/DashboardNavbar.jsx
+++ src/components/DashboardNavbar/DashboardNavbar.jsx
@@ -2,13 +2,19 @@ import { useLocation } from "react-router";
 import LogoutButton from "../LogoutButton/LogoutButton";
 import SearchInput from "../SearchInput/SearchInput";
 import styles from "./DashboardNavbar.module.css";
+import { useMemo } from "react";
 
 function DashboardNavbar({ searchValue, searchOnChange }) {
   const location = useLocation();
 
+  const isSettings = useMemo(
+    () => location.pathname.includes("settings"),
+    [location.pathname],
+  );
+
   return (
     <header className={styles.header}>
-      {location.pathname.includes("library") && (
+      {!isSettings && (
         <SearchInput value={searchValue} onChange={searchOnChange} />
       )}
       <div className={styles.logoutButton}>
