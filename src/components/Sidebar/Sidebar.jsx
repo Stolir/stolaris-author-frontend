3915016fb80b70197dbca94b15e708b0167baa92
Abdelrahman <119765066+Stolir@users.noreply.github.com>
2026-06-12 15:22:34 +0300

    Change sidebar content to stick on scroll

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -9,21 +9,27 @@ function Sidebar() {
 
   return (
     <aside className={styles.sidebar}>
-      <Link to="/author/dashboard" className={`${styles.logo}`}>
-        <p className={styles.mainText}>STOLARIS</p>
-        <p className={styles.logoSubtext}>EDITORIAL WORKSPACE</p>
-      </Link>
-      <section className={styles.navItems}>
-        <SidebarItem name={"Library"} icon={<Book />} path={"library"} />
-        <SidebarItem name={"Settings"} icon={<Settings />} path={"settings"} />
-      </section>
-      <section className={styles.controls}>
-        <CustomButton
-          text={"New Article"}
-          icon={<PagePlus />}
-          onClick={() => navigate("/author/editor")}
-        />
-      </section>
+      <div>
+        <Link to="/author/dashboard" className={`${styles.logo}`}>
+          <p className={styles.mainText}>STOLARIS</p>
+          <p className={styles.logoSubtext}>EDITORIAL WORKSPACE</p>
+        </Link>
+        <section className={styles.navItems}>
+          <SidebarItem name={"Library"} icon={<Book />} path={"library"} />
+          <SidebarItem
+            name={"Settings"}
+            icon={<Settings />}
+            path={"settings"}
+          />
+        </section>
+        <section className={styles.controls}>
+          <CustomButton
+            text={"New Article"}
+            icon={<PagePlus />}
+            onClick={() => navigate("/author/editor")}
+          />
+        </section>
+      </div>
     </aside>
   );
 }
