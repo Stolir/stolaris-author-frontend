01d6e9571b380e998a13a1dfdf922d854714244b
Abdelrahman <119765066+Stolir@users.noreply.github.com>
2026-06-13 18:01:14 +0300

    Fix issue where invalid session was not handled correctly on the client

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
-import { useLoaderData, useNavigation } from "react-router";
+import { useNavigation } from "react-router";
 
 import { AuthProvider, useAuth } from "./context/AuthContext";
 import { useEffect } from "react";
 import LoadingSpinner from "./components/LoadingSpinner/LoadingSpinner";
 import AuthLayout from "./layouts/AuthLayout/AuthLayout";
 import PublicLayout from "./layouts/PublicLayout";
+import { getUser } from "./lib/serverRequests";
 
 function AppContent() {
-  const data = useLoaderData();
-  const { login, loading, setLoading, user } = useAuth();
-  // console.log(data);
+  const { login, logout, loading, user } = useAuth();
   useEffect(() => {
-    if (data.user) {
-      login(data.user);
-    } else {
-      setLoading(false);
-    }
-  }, [data]);
+    let ignored = false;
+    (async () => {
+      const user = await getUser();
+      if (ignored) return;
+      if (user) {
+        login(user);
+      } else {
+        logout();
+      }
+    })();
+    return () => (ignored = true);
+  }, [login, logout]);
 
   if (loading) return <LoadingSpinner />;
 
